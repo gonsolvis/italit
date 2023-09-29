57a7d9d87764f70bc685e7bda3b2052de135b84e
Ross Gonsolvis <r.gonsolvis@criteo.com>
2023-09-29 19:41:30 +0200

    will add css

--- src/LevelChoice/LevelChoice.jsx
+++ src/LevelChoice/LevelChoice.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import CircularButton from '../CircularButton/CircularButton';
 import LESSONS_BREAKDOWN from '../DataFolder/LessonsBreakdown';
-import romulus from "../images/romulus.png"
 import './levelChoice.css';
 
 function LevelChoice() {
@@ -10,13 +9,11 @@ function LevelChoice() {
   return (
     <div className='LevelChoice'>
       <div className='InnerLevelChoice'> 
+      <h1 className='h1-LevelChoice'>  Choose a level</h1>
 
- <div className="card w-50 h-75">
+ <div className="card" id='chooseLevelCard'>
   
   <div className="card-body">
-  <h1> Choose your level</h1>
-  {/* <img className="home-image" src={romulus} alt="react logo" /> */}
- <img className="home-image" />
   <p >Choose below from lessons starting from A1 (Beginner) to C2 (Native).
     We will help you along the way with lessons and games!  </p> 
   </div> 
