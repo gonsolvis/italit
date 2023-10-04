82f6ef353b06e4055f7ead3977be3a35d675287f
Ross Gonsolvis <r.gonsolvis@criteo.com>
2023-10-05 00:35:04 +0200

    will fix part of the responsive issues

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import "./home.css"
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
-   
   return (
-    <div className='Home' >
-      <div className='leftside' id='leftsideId'>  
-        
-     </div>
-     <div className='rightside'>    
-     <div className='minirightside'>       
-  <h1 className='h1-home'> ItaLit</h1>
-  <p className='p-home'> Welcome to our attempt of teaching italian! I am not a native speaker and neither is chatGpt but we only try our best to teach for free those who want to learn. </p>
-   <p id='p-extraInfo'>
-    Click Below to choose the lessons from the different levels we have available! </p>
-     <Link to="/level_choice" >
-     <button className='button-home'>
-     See the levels 
-     </button>
-     </Link>
-     </div>
-     </div>
-     </div>
-        
+    <div className="Home">
+      <div className="leftside" id="leftsideId"></div>
+      <div className="rightside">
+        <div className="minirightside">
+          <h1 className="h1-home"> ItaLit</h1>
+          <p className="p-home">
+            Welcome to our attempt of teaching italian! I am not a native
+            speaker and neither is chatGpt but we only try our best to teach for
+            free those who want to learn.
+          </p>
+          <p id="p-extraInfo">
+            Click Below to choose the lessons from the different levels we have
+            available!
+          </p>
+          <Link to="/level_choice">
+            <button className="button-home">See the levels</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/LevelChoice/LevelChoice.jsx
+++ src/LevelChoice/LevelChoice.jsx
@@ -1,46 +1,51 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import CircularButton from '../CircularButton/CircularButton';
-import LESSONS_BREAKDOWN from '../DataFolder/LessonsBreakdown';
-import './levelChoice.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CircularButton from "../CircularButton/CircularButton";
+import LESSONS_BREAKDOWN from "../DataFolder/LessonsBreakdown";
+import "./levelChoice.css";
 
 function LevelChoice() {
-   
   return (
-    <div className='LevelChoice'>
-      <div className='InnerLevelChoice'> 
-    
-      <h1 className='h1-LevelChoice'>  Choose a level</h1>
+    <div className="LevelChoice">
+      <div className="InnerLevelChoice">
+        <h1 className="h1-LevelChoice"> Choose a level</h1>
 
-<p className='p-levelchoice'>Choose below from lessons starting from A1 (Beginner) to C2 (Native).
-  We will help you along the way with lessons and games!  </p> 
+        <p className="p-levelchoice">
+          Choose below from lessons starting from A1 to C2.
+          We will help you along the way with lessons and games!{" "}
+        </p>
 
-  </div>
-        <div className='buttonMappingBox'>
-       
-       {LESSONS_BREAKDOWN.map((circle, index) => (
-           <CircularButton
-           key={index}
-           title={circle.level}
-           color={circle.color}
-           levelUrl={circle.levelUrl}
-           />
-          ))}
+        <p className="p-levelchoice-extra">
+          At A1, beginners learn greetings, numbers, and basic vocabulary for
+          simple present tense sentences. A2 builds vocabulary, introduces basic
+          grammar, and enables elementary conversations. B1 adds past tenses,
+          complex topics, and improves listening and writing skills. B2 involves
+          advanced conversations, newspaper reading, and mastering the
+          subjunctive. C1 demonstrates fluency, complex literature reading, and
+          cultural mastery, while C2 showcases near-native proficiency across
+          all language aspects, opening advanced opportunities!{" "}
+        </p>
+      </div>
+      <div className="buttonMappingBox">
+        {LESSONS_BREAKDOWN.map((circle, index) => (
+          <CircularButton
+            key={index}
+            title={circle.level}
+            color={circle.color}
+            levelUrl={circle.levelUrl}
+          />
+        ))}
 
-{LESSONS_BREAKDOWN.map((circle, index) => (
-           <CircularButton
-           key={index}
-           title={circle.level}
-           color={circle.color}
-           levelUrl={circle.levelUrl}
-           />
-          ))}
-
-          
-        </div>
-        
-        </div>
-        
+        {LESSONS_BREAKDOWN.map((circle, index) => (
+          <CircularButton
+            key={index}
+            title={circle.level}
+            color={circle.color}
+            levelUrl={circle.levelUrl}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
